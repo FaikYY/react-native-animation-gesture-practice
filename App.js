9d2119c44962339329938cb52fadf6ad04d030d7
Faik YESILYAPRAK <faikyy@Faiks-MacBook-Air.local>
2021-12-22 20:42:38 -0600

    sphere added

--- App.js
+++ App.js
@@ -13,34 +13,69 @@ import {
 const SIZE = 100.0;
 
 export default function App() {
-  const translateX = useSharedValue(0);
-  const translateY = useSharedValue(0);
+  // #############
+  //  SQUARE
+  // #############
 
-  const panGestureEvent = useAnimatedGestureHandler({
+  const square_translateX = useSharedValue(0);
+  const square_translateY = useSharedValue(0);
+
+  const squarePanGestureEvent = useAnimatedGestureHandler({
     onStart: (event, context) => {
-      context.translateX = translateX.value;
-      context.translateY = translateY.value;
+      context.square_translateX = square_translateX.value;
+      context.square_translateY = square_translateY.value;
     },
     onActive: (event, context) => {
-      translateX.value = event.translationX + context.translateX;
-      translateY.value = event.translationY + context.translateY;
+      square_translateX.value = event.translationX + context.square_translateX;
+      square_translateY.value = event.translationY + context.square_translateY;
     },
     onEnd: (event) => {},
   });
 
-  const reanimatedStyle = useAnimatedStyle(() => {
+  const squareReanimatedStyle = useAnimatedStyle(() => {
     return {
       transform: [
-        { translateX: translateX.value },
-        { translateY: translateY.value },
+        { translateX: square_translateX.value },
+        { translateY: square_translateY.value },
+      ],
+    };
+  });
+
+  // #############
+  //  SPHERE
+  // #############
+
+  const sphere_translateX = useSharedValue(0);
+  const sphere_translateY = useSharedValue(0);
+
+  const spherePanGestureEvent = useAnimatedGestureHandler({
+    onStart: (event, context) => {
+      context.sphere_translateX = sphere_translateX.value;
+      context.sphere_translateY = sphere_translateY.value;
+    },
+    onActive: (event, context) => {
+      sphere_translateX.value = event.translationX + context.sphere_translateX;
+      sphere_translateY.value = event.translationY + context.sphere_translateY;
+    },
+    onEnd: (event) => {},
+  });
+
+  const sphereReanimatedStyle = useAnimatedStyle(() => {
+    return {
+      transform: [
+        { translateX: sphere_translateX.value },
+        { translateY: sphere_translateY.value },
       ],
     };
   });
 
   return (
     <GestureHandlerRootView style={styles.container}>
-      <PanGestureHandler onGestureEvent={panGestureEvent}>
-        <Animated.View style={[styles.square, reanimatedStyle]} />
+      <PanGestureHandler onGestureEvent={squarePanGestureEvent}>
+        <Animated.View style={[styles.square, squareReanimatedStyle]} />
+      </PanGestureHandler>
+      <PanGestureHandler onGestureEvent={spherePanGestureEvent}>
+        <Animated.View style={[styles.sphere, sphereReanimatedStyle]} />
       </PanGestureHandler>
       <StatusBar style="auto" />
     </GestureHandlerRootView>
@@ -59,5 +94,11 @@ const styles = StyleSheet.create({
     height: SIZE,
     backgroundColor: "rgba(0,0,256,0.5)",
     borderRadius: 20,
+  },
+  sphere: {
+    width: SIZE,
+    height: SIZE,
+    backgroundColor: "red",
+    borderRadius: 50,
   },
 });
